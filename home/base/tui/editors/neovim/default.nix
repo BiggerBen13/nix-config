6d817ec98c9bec1ab346e21ae2ac2d349729bb9e
Ben Kirschniak <ben@kirschniak.de>
2024-11-14 15:01:05 +0100

    GEN: 62 on: 2024-11-14 at: 15:00:57

--- home/base/tui/editors/neovim/default.nix
+++ home/base/tui/editors/neovim/default.nix
@@ -6,8 +6,7 @@
   ...
 }: {
   home.activation.installNvimConfig = lib.hm.dag.entryAfter ["writeBoundary"] ''
-    rm -rf ${config.xdg.configHome}/nvim
-    ln -s ${./.}/nvim ${config.xdg.configHome}/nvim
+    rm -rf ${config.xdg.configHome}/nvim && ln -s ${./.}/nvim ${config.xdg.configHome}/nvim
   ''; # --chmod=D255,F745
   # ${pkgs.rsync}/bin/rsync -avz  ${./nvim}/ ${config.xdg.configHome}/nvim/
 

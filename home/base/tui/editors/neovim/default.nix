4280b4337fb56aff2f8c1b9c4a912dceb97addb3
Ben Kirschniak <ben@kirschniak.de>
2024-11-14 20:49:43 +0100

    test

--- home/base/tui/editors/neovim/default.nix
+++ home/base/tui/editors/neovim/default.nix
@@ -4,9 +4,14 @@
   pkgs,
   pkgs-unstable,
   ...
-}: {
+}: let 
+nvim_config = lib.fetchFromGithub ({
+url = "https://github.com/BiggerBen13/nvim-config.git";
+ref = "main";
+});
+in{
   home.activation.installNvimConfig = lib.hm.dag.entryAfter ["writeBoundary"] ''
-    rm -rf ${config.xdg.configHome}/nvim && ln -s ${./.}/nvim ${config.xdg.configHome}/nvim
+    rm -rf ${config.xdg.configHome}/nvim && ln -s ${toString ./nvim} ${config.xdg.configHome}/nvim
   ''; # --chmod=D255,F745
   # ${pkgs.rsync}/bin/rsync -avz  ${./nvim}/ ${config.xdg.configHome}/nvim/
 

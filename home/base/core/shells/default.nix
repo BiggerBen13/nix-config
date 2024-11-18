92176f298ddcfce1ee62f505475bc39d3b370016
Ben Kirschniak <ben@kirschniak.de>
2024-11-18 22:42:20 +0100

    GEN: 86 on: 2024-11-18 at: 22:42:13

--- home/base/core/shells/default.nix
+++ home/base/core/shells/default.nix
@@ -5,10 +5,13 @@
 }: let
   localBin = "${config.home.homeDirectory}/.local/bin";
   rustBin = "${config.home.homeDirectory}/.cargo/bin";
+  brewBin = "/opt/homebrew/bin";
 in {
   programs.bash = {
     enable = true;
     enableCompletion = true;
-    bashrcExtra = ''export PATH=$PATH:${localBin}:${rustBin}'';
+    bashrcExtra = ''export PATH=$PATH:${localBin}:${rustBin}:brewBin'';
   };
+
+  # programs.fish.shellInit = ''set PATH $PATH'';
 }

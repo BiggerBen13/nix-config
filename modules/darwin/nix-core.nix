cc181e2201538359b0d4715d2b151a409c81d7a2
Ben Kirschniak <ben@kirschniak.de>
2024-11-25 17:12:25 +0100

    GEN: [126] on: [2024-11-25] at: [17:12:16] note: added trying to get x86 to work

--- modules/darwin/nix-core.nix
+++ modules/darwin/nix-core.nix
@@ -7,7 +7,23 @@
 
   nix.gc.automatic = false;
 
-  nix.linux-builder.enable = true;
+  nix.linux-builder = {
+    enable = true;
+    maxJobs = 4;
+    config = {
+      virtualisation = {
+        darwin-builder = {
+          diskSize = 40 * 1024;
+          memorySize = 8 * 1024;
+        };
+        cores = 6;
+      };
+    };
+    systems = [
+      "x86_64-linux"
+      "aarch64-linux"
+    ];
+  };
 
   system.stateVersion = 5;
 }
